6398652b63d86bae65a1a007220973fdd6f7e4e4
Prosenjit88 <41550771+Prosenjit88@users.noreply.github.com>
2019-07-29 19:42:39 +0200

    Add files via upload

--- model/create_tfrecords_from_xml.py
+++ model/create_tfrecords_from_xml.py
@@ -51,8 +51,6 @@ def dict_to_tf_example(data, image_dir, label_map_dict):
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = PIL.Image.open(encoded_jpg_io)
-    if image.format != 'JPEG':
-        raise ValueError('Image format not JPEG')
     key = hashlib.sha256(encoded_jpg).hexdigest()
 
     width = int(data['size']['width'])

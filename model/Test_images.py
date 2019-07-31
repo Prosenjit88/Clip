88a72aaf8b1195f157b2be999222a787c3ff8e4f
Prosenjit88 <41550771+Prosenjit88@users.noreply.github.com>
2019-07-31 21:47:20 +0200

    Add files via upload

--- model/Test_images.py
+++ model/Test_images.py
@@ -35,10 +35,6 @@ with detection_graph.as_default():
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
 
 label_map = label_map_util.load_labelmap(args["label"])
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=1, use_display_name=True)
@@ -46,7 +42,8 @@ category_index = label_map_util.create_category_index(categories)
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
-    image_np = load_image_into_numpy_array(Image.open(args["image"]))
+    image_np = cv2.imread(args["image"])
+    image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
     boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
     scores = detection_graph.get_tensor_by_name('detection_scores:0')
